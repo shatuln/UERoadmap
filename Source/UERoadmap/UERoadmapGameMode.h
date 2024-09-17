a225a08529b7771de79f05f42b4f0bb683a5a305
Leonid Shatunov <shatuln@gmail.com>
2024-09-18 00:06:37 +0200

    Initial commit

---
+++ Source/UERoadmap/UERoadmapGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UERoadmapGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUERoadmapGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUERoadmapGameMode();
+};
+
+
+
